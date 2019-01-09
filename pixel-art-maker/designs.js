bb1110dafc6c1a0dc971b0a42110d6434983cc2b
Somsak Chhom <somsak.chhom@gmail.com>
2019-01-09 11:05:16 -0800

    nested for loops for grid

--- pixel-art-maker/designs.js
+++ pixel-art-maker/designs.js
@@ -2,9 +2,26 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+// make grid
 function makeGrid() {
 
-// Your code goes here!
+  // gridHeight
+  const gridHeight = 5;
 
+  // gridWidth
+  const gridWidth = 5;
+
+  // for each number up to gridHeight
+  for (let row = 0; row < gridHeight; row++) {
+
+    // add row to table element
+    console.log('adding row to table');
+
+    // for each number up to gridWidth
+    for (let td = 0; td < gridWidth; td++) {
+
+      // add td to row element
+      console.log('adding td to row');
+    }
+  }
 }
